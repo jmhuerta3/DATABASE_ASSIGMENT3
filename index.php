c3e9eb8df4ffc11ab05018361bf1246978f8d4bc
mqvu <157411443+mqvu@users.noreply.github.com>
2024-11-25 14:22:26 -0700

    changing index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!--
-/**
- * CS 4342 Database Management
- * @author Instruction team Spring and Fall 2020 with contribution from L. Garnica
- * @version 2.0
- * Description: The purpose of these file is to provide PhP basic elements for an interface to access a DB. 
- * Resources: https://getbootstrap.com/docs/4.5/components/alerts/  -- bootstrap examples
- * Include your name here - ex. Modified by Villanueva for Assignment 2
- */
--->
-
 <!doctype html>
 <html lang="en">
 
@@ -20,69 +9,56 @@
   <!-- Bootstrap CSS library https://getbootstrap.com/ -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+  <style>
+    body {
+      background-color: #f0f8ff; /* Light blue background */
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    .button-group button {
+      border: 2px solid black;
+      background-color: #f0f8ff;
+      color: black;
+      width: 200px;
+      font-weight: bold;
+      margin: 10px 0;
+    }
+
+    .button-group button:hover {
+      background-color: #f0f8ff; /* Same as body background */
+      color: black;
+      cursor: pointer;
+    }
+  </style>
 </head>
 
-<body style="background-color: #f0f8ff;"> <!-- Light blue background -->
-  <div style="margin-top: 20px" class="container">
-    
+<body>
+  <div class="container">
     <h1>Welcome!</h1>
-    
+
     <div class="button-group">
-      <button class="btn btn-primary" onclick="location.href='login_organization.php'">
-        Admin Login 
+      <button class="btn" onclick="location.href='login_organization.php'">
+        Admin Login
       </button>
-      <br><br>
-      <button class="btn btn-secondary" onclick="location.href='login_opportunities.php'">
+      <button class="btn" onclick="location.href='login_opportunities.php'">
         View Opportunities
       </button>
-      <br><br>
-      <button class="btn btn-success" onclick="location.href='login_representative.php'">
+      <button class="btn" onclick="location.href='login_representative.php'">
         Representative Login
       </button>
     </div>
-    
   </div>
-</body>
-
 
   <!-- jQuery and JS bundle w/ Popper.js -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-
-</>
+</body>
 
 </html>
-
-
-<?php
-
-session_start();
-require_once("config.php");
-$_SESSION['logged_in'] = false;
-
-if (!empty($_POST)) {
-  if (isset($_POST['Submit'])) {
-    $input_username = isset($_POST['username']) ? $_POST['username'] : " ";
-    $input_password = isset($_POST['password']) ? $_POST['password'] : " ";
-
-    $queryUser = "SELECT * FROM User  WHERE Uusername='" . $input_username . "' AND UPassword='" . $input_password . "';";
-    $resultUser = $conn->query($queryUser);
-
-    if ($resultUser->num_rows > 0) {
-      //if there is a result, that means that the user was found in the database
-      $_SESSION['user'] = $input_username;
-      $_SESSION['logged_in'] = true;
-      
-      echo "Session logged_in is: ".$_SESSION['logged_in'];
-      
-      // You can comment the next line (header) to check if the user was successfully logged in. 
-      // But it will not redirect to the student_menu file automatically.
-      header("Location: studentsCode/student_menu.php");
-    } else {
-      echo "User not found.";
-    }
-    die();
-  }
-}
-?>
